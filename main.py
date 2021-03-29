1cb25d03b8aa09f8a8357cb4ad53076231ae1570
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2021-03-30 01:03:29 +0700

    add dev and help commands

--- main.py
+++ main.py
@@ -22,6 +22,14 @@ async def on_message(message):
     if message.content.startswith('$invite-me'):
         await message.channel.send('Add this bot to your discord server by clicking this link: https://discord.com/api/oauth2/authorize?client_id=825996658554568725&permissions=2148002880&scope=bot')
 
+    if message.content.startswith('$dev'):
+        await message.channel.send('**IBPME-Bot** is developed by **Iman-Budi Pranakasih**\nTo help contribute view this repo on github: https://github.com/ibpme/IBPME-DiscordBot')
+
+    if message.content.startswith('$help') or message.content.startswith('$?'):
+        with open('commands.png', 'rb') as f:
+            picture = discord.File(f)
+        await message.channel.send('More info on : https://github.com/ibpme/IBPME-DiscordBot/blob/main/README.md', file=picture)
+
     # NIM FINDER
     if message.content.startswith("$find-name"):
         nim = message.content.split("$find-name", 1)[1].strip()
