5d586f6175202b7c6273acbca49b8ce61f2b3cca
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2021-03-30 00:45:51 +0700

    add invite me link

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # GENERAL COMMANDS
     if message.content.startswith('$hello'):
         await message.channel.send('Hello {}!'.format(message.author.name))
 
     if message.content.startswith('$invite-me'):
-        await message.channel.send('Add this bot to yout discord server by clicking this link:')
+        await message.channel.send('Add this bot to your discord server by clicking this link: https://discord.com/api/oauth2/authorize?client_id=825996658554568725&permissions=2148002880&scope=bot')
 
+    # NIM FINDER
     if message.content.startswith("$find-name"):
         nim = message.content.split("$find-name", 1)[1].strip()
         name = nim_api.find_name(nim)
