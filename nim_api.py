1fa4a5f0161744178d495f3c4c27d43f12275783
Garry Kuwanto <gkuwanto@gmail.com>
2021-04-01 01:09:54 +0700

    change name search to use fuzzy algorithm

--- nim_api.py
+++ nim_api.py
@@ -1,4 +1,5 @@
 import json
+from rapidfuzz import fuzz
 
 with open("nim_to_name.json", 'r') as file:
     nim_to_name = json.load(file)
@@ -21,13 +22,17 @@ def find_nim(name):
             name_to_nim[y] = [x, name_to_nim[y]]
             continue
         name_to_nim[y] = x
-    try:
-        nim = name_to_nim[name]
-    except KeyError:
-        return "No nim found for {}! Make sure to type the correct full name".format(name)
-    if type(nim) == list:
-        return "**TPB**: {}\n**Jurusan**: {}".format(nim[0], nim[1])
-    return nim
+    
+    candidates = []
+    for element in name_to_nim:
+        if fuzz.partial_ratio(name.lower(), element.lower(), score_cutoff=90):
+            candidates.append((element, name_to_nim[element]))
+    if len(candidates) == 0 :
+        return "No nim found for {}!".format(name)
+    output_string = ''
+    for name, nim in candidates:
+        output_string += f'**Name**: {name}\n**TPB**: {nim[0]}\n**Jurusan**: {nim[1]} \n\n'
+    return output_string
 
 
 def make_nim_to_name_hash():
